c31a6396472f269f5fd7bef965fe74864b16046e
bahromoken <magdiyevbahrom@gmail.com>
2023-12-14 22:57:32 +0500

    test for tag models title

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,16 @@
+from app.models import Tag, Post
 from django.test import TestCase
 
-# Create your tests here.
+class TagTest(TestCase):
+    def setUp(self):
+        Tag.objects.create(title='Telegram')
+        Tag.objects.create(title='Twitter')
+
+    def test_socials_title(self):
+        tag_named_telegram = Tag.objects.get(title='Telegram')
+        tag_named_twitter = Tag.objects.get(title='Twitter')
+
+        self.assertEqual(tag_named_telegram.tag_name(), 'Telegram')
+        self.assertEqual(tag_named_twitter.tag_name(), 'Twitter')
+
+
